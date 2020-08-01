07035c5fceafb7e25694269b37bb6ee10cc0e5b1
manurathi1 <manurathi.jaipur@gmail.com>
2020-08-01 13:35:56 +0530

    intial commit

---
+++ awm_pgdba/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for awm_pgdba project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'awm_pgdba.settings')
+
+application = get_wsgi_application()
