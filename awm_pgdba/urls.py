5df58bb54d82475653939abbd8154aa066f30f7f
manurathi1 <manurathi.jaipur@gmail.com>
2020-08-03 04:17:18 +0530

    added initial landing app with relevant files

--- awm_pgdba/urls.py
+++ awm_pgdba/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from landing.views import landing_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_view, name = "landing-home")
 ]
